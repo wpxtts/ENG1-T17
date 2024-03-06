8e3a11f8b5749c63e28644df160786f37ceb0312
wpxtts <127058679+wpxtts@users.noreply.github.com>
2024-03-06 12:44:28 +0000

    Block on screen
    Added a red block on the screen to represent the sprite.

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -4,23 +4,42 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.graphics.Color;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	private ShapeRenderer shapeRenderer;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		shapeRenderer = new ShapeRenderer();
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(1, 1, 1, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+		shapeRenderer.begin(ShapeType.Filled);
+
+		// Set the color to red
+		shapeRenderer.setColor(Color.RED);
+
+		// Draw a small red block at position (x, y) with width and height of 10 pixels
+		float x = 100;
+		float y = 100;
+		float width = 50;
+		float height = 50;
+		shapeRenderer.rect(x, y, width, height);
+
+		// End rendering shapes
+		shapeRenderer.end();
 	}
 	
 	@Override
