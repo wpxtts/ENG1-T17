b888cbe9fd91808387aa592c5f15e5068e1bb710
wpxtts <127058679+wpxtts@users.noreply.github.com>
2024-03-07 08:50:57 +0000

    Adding Movement
    Adding movement controls to the red block using the arrow keys.

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,6 +1,7 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -8,15 +9,20 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.Input.Keys;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	private ShapeRenderer shapeRenderer;
 	private OrthographicCamera camera;
-	
+	private float x = 100;
+	private float y = 100;
+	private float width = 100;
+	private float height = 100;
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		shapeRenderer = new ShapeRenderer();
@@ -25,31 +31,46 @@ public class MyGdxGame extends ApplicationAdapter {
 	}
 
 	@Override
-	public void render () {
+	public void render() {
 		ScreenUtils.clear(1, 1, 1, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+		shapeRenderer.setProjectionMatrix(camera.combined);
 		shapeRenderer.begin(ShapeType.Filled);
 
 		// Set the color to red
 		shapeRenderer.setColor(Color.RED);
 
 		// Draw a small red block at position (x, y) with width and height of 10 pixels
-		float x = 100;
-		float y = 100;
-		float width = 100;
-		float height = 100;
 		shapeRenderer.rect(x, y, width, height);
 
 		// End rendering shapes
 		shapeRenderer.end();
 		camera.update();
+		handleInput();
 	}
-	
+
+	private void handleInput() {
+		// Move the block with arrow keys
+		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+			x -= 200 * Gdx.graphics.getDeltaTime();
+		}
+		if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			x += 200 * Gdx.graphics.getDeltaTime();
+		}
+		if (Gdx.input.isKeyPressed(Keys.UP)) {
+			y += 200 * Gdx.graphics.getDeltaTime();
+		}
+		if (Gdx.input.isKeyPressed(Keys.DOWN)) {
+			y -= 200 * Gdx.graphics.getDeltaTime();
+		}
+	}
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
 		img.dispose();
+		shapeRenderer.dispose();
 	}
 }
