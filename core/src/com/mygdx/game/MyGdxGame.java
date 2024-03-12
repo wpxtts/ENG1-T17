543b3192abcf09f7ff0d1322721679b2ff43328f
Se1towo <zot500@york.ac.uk>
2024-03-12 12:59:38 +0000

    Adding buttons
    Started working on buttons by implementing Stage

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,15 +3,21 @@ package com.mygdx.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.Gdx;
 public class MyGdxGame extends Game {
 
 	public SpriteBatch batch;
 	public BitmapFont font;
+	private Stage stage;
 
 	public void create() {
 		batch = new SpriteBatch();
 		font = new BitmapFont(); // use libGDX's default Arial font
 		this.setScreen(new MenuScreen(this));
+		//initialises a cover for the entire screen to enable event capture
+		stage = new Stage(new ScreenViewport());
+		Gdx.input.setInputProcessor(stage);
 	}
 
 	public void render() {
