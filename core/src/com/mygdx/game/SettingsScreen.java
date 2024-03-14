11d7abbb28fc4b05301fbe038d98cba977005679
wpxtts <127058679+wpxtts@users.noreply.github.com>
2024-03-14 23:49:54 +0000

    Sound Settings
    sound settings button done, settings page fundamentally done too

--- core/src/com/mygdx/game/SettingsScreen.java
+++ core/src/com/mygdx/game/SettingsScreen.java
@@ -2,53 +2,95 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 public class SettingsScreen implements Screen {
 
     final MyGdxGame game;
-
     OrthographicCamera camera;
-    Texture CreditsImg;
-    Texture SettingsImg;
+    Stage stage;
+    boolean isSoundEnabled = true; // Initial state
 
     public SettingsScreen(final MyGdxGame game) {
         this.game = game;
-        CreditsImg = new Texture("button1.png");
-        SettingsImg = new Texture("button1.png");
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
+        stage = new Stage(new StretchViewport(800, 480, camera));
+        Gdx.input.setInputProcessor(stage);
     }
 
-    //Screen interface does not provide any sort of create() method, so a constructor is used
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(0, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
 
-        game.batch.begin();
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
 
-        //Writes text to screen using game.font.draw(SpriteBatch, String, float, float)
-        game.font.draw(game.batch, "SETTINGS", 100, 150);
-
-        //Draws in Menu buttons
-        game.batch.setProjectionMatrix(camera.combined);
-        game.batch.end();
     }
 
     @Override
     public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
     }
+
     @Override
     public void show() {
+        Label.LabelStyle labelStyle = new Label.LabelStyle();
+        labelStyle.font = game.font;
+        labelStyle.fontColor = Color.WHITE;
+
+        Label titleLabel = new Label("Settings", labelStyle);
+        titleLabel.setFontScale(2);
+        titleLabel.setPosition(100, 400, Align.center);
+        stage.addActor(titleLabel);
+
+        TextButton.TextButtonStyle buttonStyle = new TextButton.TextButtonStyle();
+        buttonStyle.font = game.font;
+        buttonStyle.fontColor = Color.WHITE;
+
+        final TextButton soundButton = new TextButton("Sound: ON", buttonStyle);
+        soundButton.setPosition(100, 300);
+        soundButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
+                isSoundEnabled = !isSoundEnabled; // Toggle sound state
+                if (isSoundEnabled) {
+                    soundButton.setText("Sound: ON");
+                    soundButton.getLabel().setColor(Color.WHITE);
+                } else {
+                    soundButton.setText("Sound: OFF");
+                    soundButton.getLabel().setColor(Color.RED);
+                }
+            }
+        });
+        stage.addActor(soundButton);
+
+        TextButton backButton = new TextButton("Back", buttonStyle);
+        backButton.setPosition(100, 200);
+        backButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
+                game.setScreen(new MenuScreen(game));
+            }
+        });
+        stage.addActor(backButton);
     }
 
     @Override
     public void hide() {
+        dispose();
     }
 
     @Override
@@ -61,6 +103,6 @@ public class SettingsScreen implements Screen {
 
     @Override
     public void dispose() {
+        stage.dispose();
     }
-
 }
