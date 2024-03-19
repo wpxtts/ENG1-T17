d425d7005e5be2cb1929fe7315f86d6f45cc2c17
ancs29 <alexstaicu628@gmail.com>
2024-03-19 12:00:25 +0000

    Implemented systems, entities, component and service for displaying ugly text

---
+++ core/src/com/mygdx/game/entities/TrackerTime.java
@@ -0,0 +1,8 @@
+package com.mygdx.game.entities;
+
+public class TrackerTime extends Tracker {
+
+    public TrackerTime(String text, float x, float y, float scale) {
+        super(text, x, y, scale);
+    }
+}
