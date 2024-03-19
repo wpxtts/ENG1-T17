e884fb23e7dd97089219d758636f95ec7d2a28ec
George Jopson <64833916+GeorgeJopson@users.noreply.github.com>
2024-03-19 00:48:26 +0000

    Comment MyGdxGame
    Added Javadoc to MyGdxGame

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -7,38 +7,63 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
+/**
+ * The main class which manages the loop of the game.
+ */
 public class MyGdxGame extends Game {
 
 	public SpriteBatch batch;
 	public BitmapFont font;
 	private Stage stage;
 
+	/**
+	 * Initialises sprite batch, font and sets intial screen to MenuScreen.
+	 */
 	public void create() {
 		batch = new SpriteBatch();
 		font = new BitmapFont(); // use libGDX's default Arial font
 		this.setScreen(new MenuScreen(this));
 	}
 
+	/**
+	 * Renders the screen
+	 */
 	public void render() {
 		super.render(); // important!
 	}
 
+	/**
+	 * Clear up screen from leftover sprites and text
+	 */
 	public void dispose() {
 		batch.dispose();
 		font.dispose();
 	}
 
+	/**
+	 * Change screen to CreditScreen
+	 */
 	public void toCreditScreen(){
 		this.setScreen(new CreditScreen(this));
 	}
+
+	/**
+	 * Change screen to GameScreen
+	 */
 	public void toGameScreen(){
 		this.setScreen(new GameScreen(this));
 	}
 
+	/**
+	 * Change screen to MenuScreen
+	 */
 	public void toMainMenu(){
 		this.setScreen(new MenuScreen(this));
 	}
 
+	/**
+	 * Change to TutorialScreen
+	 */
 	public void toTutorialScreen(){
 		this.setScreen(new TutorialScreen(this));
 	}
