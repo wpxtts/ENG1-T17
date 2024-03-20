f8cee339a8806667ce6f73be020d61abbff0bd0a
ancs29 <alexstaicu628@gmail.com>
2024-03-20 12:29:37 +0000

    Added comments to some entities

--- core/src/com/mygdx/game/entities/TrackerText.java
+++ core/src/com/mygdx/game/entities/TrackerText.java
@@ -10,7 +10,21 @@ import com.mygdx.game.serviceProviders.TextEffectProvider;
 
 import java.util.HashMap;
 
+/**
+ * TrackerText is an entity representing the text displaying a tracker
+ */
+
 public class TrackerText extends Entity {
+
+    /**
+     * Constructs a TrackerText entity which hvrusdihbxhvbsFUEHWDVBFEJCBDUFBRJ.
+     * @param targetTracker The name of the value being displayed by this entity.
+     * @param batch The sprite batch which will draw the text.
+     * @param font The font used to write the text.
+     * @param x The x coordinate where the text will be written.
+     * @param y The y coordinate where the text will be written.
+     * @param scale Variable determining the size of the text.
+     */
     public TrackerText(String targetTracker, SpriteBatch batch, BitmapFont font, float x, float y, float scale) {
         this.components = new HashMap<>();
         this.addComponent(new ComponentTrackerPointer(targetTracker));
